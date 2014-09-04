65d81088e4e03974754dd54d7e2569ead012b2d3
niku1987 <navdeepst.1987@gmail.com>
2014-09-05 01:18:06 +1000

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = pg_connect("host=ec2-54-204-35-114.compute-1.amazonaws.com   dbname=dbam19skhoh0fu     user=vuhbpfyiutxfgu    password=8zz2hhonBRPjl8wPPY2wyG-dQN") or die("Could not connect:".pg_last_error());
+$con = pg_connect("host=ec2-54-197-241-96.compute-1.amazonaws.com  dbname=dsid2tcjtl6nq    user= ntgkjqbatkjsbv   password=SnqBM0mgxMKwzuNbuhZKdADzUl") or die("Could not connect:".pg_last_error());
 
 ?>
